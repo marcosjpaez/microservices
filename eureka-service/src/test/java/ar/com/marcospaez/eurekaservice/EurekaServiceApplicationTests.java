d5cdd51c9a1fafb21263d649037352faa1dbef42
Marcos <marcos@marcospaez.com.ar>
2023-06-27 16:04:52 -0300

    eureka

---
+++ eureka-service/src/test/java/ar/com/marcospaez/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.marcospaez.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
